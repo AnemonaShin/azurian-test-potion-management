bd4496a66a027b60acdfabd482a784f638a242f5
AnemonaShin <cramireza1997@gmail.com>
2026-06-08 16:58:42 -0400

    First Push

---
+++ potion/src/main/java/cl/management/potion/PotionApplication.java
@@ -0,0 +1,13 @@
+package cl.management.potion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotionApplication.class, args);
+	}
+
+}
